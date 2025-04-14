54f72c3482b6abad48ab808ba32aff6c9cb5978f
Demian <demian.caivano@gmail.com>
2025-04-14 17:29:37 +0000

    generador simple terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 import "bootstrap";
 import "./style.css";
 
+let palos = ["♣", "♠", "♥", "♦"];
+let numeros = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let paloRandom = palos[Math.floor(Math.random() * palos.length)];
+let numeroRandom = numeros[Math.floor(Math.random() * numeros.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const palosHTML = document.querySelectorAll(".paloGeneradoRandom");
+
+palosHTML.forEach(span => {
+  span.textContent = paloRandom;
+});
+
+document.querySelector(".numeroGeneradoRandom").textContent = numeroRandom;
+
+if (paloRandom === "♦" || paloRandom === "♥") {
+  palosHTML.forEach(span => {
+    span.classList.add("rojo");
+  });
+}
